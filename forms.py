f01aa31cda604d7badfebb9d95d68c59dcc9770d
Haozhe Chen <chz_chen@outlook.com>
2020-05-06 14:43:41 +0800

    v0.1 original version

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Password Repeat", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
+
+
